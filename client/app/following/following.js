db8e08eeaa798be26c10007a6a5e5581ebcfecc2
Purit <tunesudro@hotmail.com>
2019-05-13 22:23:23 +0700

    HW5: Add following/unfollowing feature (Is removeFollowing in DNSW server bug?)

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -9,9 +9,24 @@ angular.module('followingList', ['ngRoute'])
       const requestOptions = {
           headers: { 'X-session': $rootScope.x_session }
       };
-
+       
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
-      });
+        });
+        self.sendFollow = function sendFollow(followingname) {
+            
+            const data = "followingname=" + encodeURIComponent(followingname);
+            $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions).then(function (response) {
+                console.log(response);
+            });
+        }
+        self.sendUnFollow = function sendUnFollow(followingname) {
+            $http.defaults.headers.delete = { 'X-session': $rootScope.x_session, "Content-Type": "application/json;charset=utf-8" };
+            const data = "followingname=" + encodeURIComponent(followingname);
+            console.log(data);
+            $http.delete('http://localhost:8080/twitterapi/following/?' + data).then(function (response) {
+                console.log(response);
+            });
+        }
     }]
 });
